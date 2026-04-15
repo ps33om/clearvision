45357bd34b8eb570ee6e45b1bcade18cde65da3a
ps33om <ps33.om@gmail.com>
2026-04-15 11:21:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'seeing-clearly-v1';
-const ASSETS = ['./vision21-pwa.html', './manifest.json'];
+const CACHE = 'seeing-clearly-v2';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
